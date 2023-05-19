23a2dfd08ca0a4d4f80c8fc7857c3c08e9f48a5f
Ali Burhan Güncan <33aliburhan@gmail.com>
2023-05-20 01:45:58 +0300

    Search işlemi tamamlandı.

--- Blog.Web.Mvc/Controllers/BlogController.cs
+++ Blog.Web.Mvc/Controllers/BlogController.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Blog.Web.Mvc.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +13,28 @@ namespace Blog.Web.Mvc.Controllers
 {
     public class BlogController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Search(string query, int page)
+        private readonly AppDbContext _context;
+
+        public BlogController(AppDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Search(string query, int? page)
+        {
+            var categoryNames = _context.Categories.Include(e => e.Posts).ToList();
+            ViewBag.CategoryNames = categoryNames;
+
+            var posts = _context.Posts
+                .Include(e => e.Category)
+                .Select(e => e);
+
+            if (query != null) posts = posts.Where(e => e.Content.Contains(query) || e.Title.Contains(query));
+
+            var postList = posts.ToList();
+
+            return View(postList);
         }
 
         public IActionResult Detail(int id)
